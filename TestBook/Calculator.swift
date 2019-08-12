67cb6aacedf9aba8ade50acbbc3441e04283862d
ks-cap <capricorn090114@gmail.com>
2019-08-12 21:33:12 +0900

    四則演算クラス作成

---
+++ TestBook/Calculator.swift
@@ -0,0 +1,25 @@
+//
+//  Calculator.swift
+//  TestBook
+//
+//  Created by 佐藤 賢 on 2019/08/12.
+//  Copyright © 2019 佐藤 賢. All rights reserved.
+//
+
+class Calculator {
+    func add(_ x: Int, _ y: Int) -> Int {
+        return x + y
+    }
+    
+    func subtract(_ x: Int, _ y: Int) -> Int {
+        return x - y
+    }
+    
+    func multiple(_ x: Int, _ y: Int) -> Int {
+        return x * y
+    }
+    
+    func division(_ x: Int, _ y: Int) -> Int {
+        return x / y
+    }
+}
